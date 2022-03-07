d283f6cf3b96c520665a4cbd86dcc4d3a0a44472
tomato566 <100208839+tomato566@users.noreply.github.com>
2022-03-07 19:59:31 +0800

    Add files via upload

---
+++ js/thumbnail.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage = document.querySelector("#smart-image1");
+    
+    targetImage.addEventListener("click", function() {
+        if (targetImage.classList.contains("small")) {
+            targetImage.classList.remove("small");
+        } else {
+            targetImage.classList.add("small");
+        }
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage = document.querySelector("#smart-image2");
+    
+    targetImage.addEventListener("click", function() {
+        if (targetImage.classList.contains("small")) {
+            targetImage.classList.remove("small");
+        } else {
+            targetImage.classList.add("small");
+        }
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage = document.querySelector("#smart-image3");
+    
+    targetImage.addEventListener("click", function() {
+        if (targetImage.classList.contains("small")) {
+            targetImage.classList.remove("small");
+        } else {
+            targetImage.classList.add("small");
+        }
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage = document.querySelector("#smart-image4");
+    
+    targetImage.addEventListener("click", function() {
+        if (targetImage.classList.contains("small")) {
+            targetImage.classList.remove("small");
+        } else {
+            targetImage.classList.add("small");
+        }
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage = document.querySelector("#smart-image5");
+    
+    targetImage.addEventListener("click", function() {
+        if (targetImage.classList.contains("small")) {
+            targetImage.classList.remove("small");
+        } else {
+            targetImage.classList.add("small");
+        }
+    });
+
+});
